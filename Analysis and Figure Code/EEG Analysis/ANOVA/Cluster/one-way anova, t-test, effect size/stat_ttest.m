b810a5f970e7629a5b96c263dafa9517f0b476e8
Srijita Karmakar <122189878+srijitakarmakar@users.noreply.github.com>
2026-02-08 22:23:35 -0800

    Update stat_ttest.m

--- Analysis and Figure Code/EEG Analysis/ANOVA/Cluster/one-way anova, t-test, effect size/stat_ttest.m
+++ Analysis and Figure Code/EEG Analysis/ANOVA/Cluster/one-way anova, t-test, effect size/stat_ttest.m
@@ -2,10 +2,11 @@
 load 'N250_left.mat'
 
 %column numbers of t-test comparison
-y = erpvalues(:,[1 2]);
+y = erpval(:,[1 2]);
 X1 = y(:,1); 
 X2 = y(:,2); 
 [h,p,ci,stats] = ttest(X1,X2,'Tail', 'right');
 pval = p
 
 d = computeCohen_d(X1,X2,'paired')
+
